81e30a15c7492bd254ff3fe1829e7e117ce0a057
4t0n <46654249+4t0n@users.noreply.github.com>
2020-08-11 23:37:39 +0300

    Excel?

--- BugTrackingSystem/BugTrackingSystem/Form1.Designer.cs
+++ BugTrackingSystem/BugTrackingSystem/Form1.Designer.cs
@@ -152,6 +152,7 @@
             this.button6.TabIndex = 9;
             this.button6.Text = "Добавить";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // bnGetUsers
             // 
--- BugTrackingSystem/BugTrackingSystem/Form1.cs
+++ BugTrackingSystem/BugTrackingSystem/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 namespace BugTrackingSystem
 {
     public partial class Form1 : Form
@@ -48,6 +49,37 @@ namespace BugTrackingSystem
             {                
                 tbList.Text += project.Name + '\r' + '\n';
             }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Excel.Application ex = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Workbook workBook = ex.Workbooks.Add(Type.Missing);
+            ex.SheetsInNewWorkbook = 3;            
+            Excel.Worksheet tasks = (Excel.Worksheet)ex.Worksheets.get_Item(1);
+            Excel.Worksheet projects = (Excel.Worksheet)ex.Worksheets.get_Item(2);
+            Excel.Worksheet users = (Excel.Worksheet)ex.Worksheets.get_Item(3);
+            tasks.Name = "Задачи";
+            projects.Name = "Проекты";
+            users.Name = "Пользователи";
+            tasks.Cells[1, 1] = String.Format("Boom {0} {1}", 1, 1);
+            Excel.Range forYach = tasks.Cells[1, 1] as Excel.Range;
+            string yach = forYach.Value2.ToString();
+            tasks.Cells[2, 1] = String.Format(yach, 2, 1);
+
+            int i = 1;
+            Excel.Range forYac = tasks.Cells[i, 1] as Excel.Range;            
+            while (forYac.Text != String.Empty)
+            {
+                forYac = tasks.Cells[i, 1] as Excel.Range;
+                i++;
+            }
+            tasks.Cells[i-1, 1] = "Boom";
+            
+            ex.Application.ActiveWorkbook.SaveAs("doc.xlsx", Type.Missing,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Excel.XlSaveAsAccessMode.xlNoChange,
+            Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);            
+            ex.Visible = true;
         }
     }
 }
